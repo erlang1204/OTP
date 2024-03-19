fc3f3c496e58a95bd6ee12260a1c2418d88673eb
erlang1204 <erlangbayu2@gmail.com>
2024-03-19 13:31:09 +0700

    first commit

---
+++ endpoint/login.php
@@ -0,0 +1,55 @@
+<?php
+include ('../conn/conn.php');
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $status = $_POST['status'];
+
+    $stmt = $conn->prepare("SELECT `password`,`status` FROM `tbl_user` WHERE `username` = :username");
+    $stmt->bindParam(':username', $username);
+    $stmt->execute();
+
+    if ($stmt->rowCount() > 0) {
+        $row = $stmt->fetch();
+        $stored_password = $row['password'];
+        $stored_status = $row['status'];
+
+        if ($stored_status === 'yes')
+        {
+            if (md5($password) === $stored_password) {
+                echo "
+                <script>
+                    alert('Login Successfully!');
+                    window.location.href = 'http://localhost/otp/home.php';
+                </script>
+                "; 
+            } else  {
+                echo "
+                <script>
+                    alert('Login Failed, Incorrect Password!');
+                    window.location.href = 'http://localhost/otp/login.php';
+                </script>
+                ";
+            }
+        } else if($stored_status === 'no')
+        {
+                echo "
+                <script>
+                    alert('Belum verifikasi!');
+                    window.location.href = 'http://localhost/otp/index.php';
+                </script>
+                "; 
+        }
+        
+    } else {
+        echo "
+            <script>
+                alert('Login Failed, User Not Found!');
+                window.location.href = 'http://localhost/otp/index.php';
+            </script>
+            ";
+    }
+}
+
+?>
---
+++ forgot-password.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Forgot Password</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<style>
+
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap');
+
+* {
+    margin: 0;
+    padding: 0;
+    font-family: 'Poppins', sans-serif;
+}
+
+body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-image: url("https://images.unsplash.com/photo-1485470733090-0aae1788d5af?q=80&w=1517&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    height: 100vh;
+}    .container{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+.container .form{
+    backdrop-filter: blur(100px);
+            color: rgb(255, 255, 255);
+            padding: 40px;
+            width: 500px;
+            border: 2px solid;
+            border-radius: 10px;
+}
+.container .form form .form-control{
+    height: 40px;
+    font-size: 15px;
+}
+.container .form form .forget-pass{
+    margin: -15px 0 15px 0;
+}
+.container .form form .forget-pass a{
+   font-size: 15px;
+}
+.container .form form .button{
+    background: #6665ee;
+    color: #fff;
+    font-size: 17px;
+    font-weight: 500;
+    transition: all 0.3s ease;
+}
+.container .form form .button:hover{
+    background: #5757d1;
+}
+.container .form form .link{
+    padding: 5px 0;
+}
+.container .form form .link a{
+    color: #6665ee;
+}
+.container .login-form form p{
+    font-size: 14px;
+}
+.container .row .alert{
+    font-size: 14px;
+}
+
+</style>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 offset-md-4 form">
+                <form action="forgot-password.php" method="POST" autocomplete="">
+                    <h2 class="text-center">Forgot Password</h2>
+                    <p class="text-center">Enter your email address</p>
+                    
+                    <div class="form-group">
+                        <input class="form-control" type="email" name="email" placeholder="Enter email address" required>
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control button" type="submit" name="check-email" value="Continue">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
